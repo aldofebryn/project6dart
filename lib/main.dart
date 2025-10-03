f5cfffa62dd184a55dd4b37f2012d0cb9f854559
alfbrnsy <aldofebriansyah0224@gmail.com>
2025-10-03 10:48:14 +0700

    BottomNavigationBar

--- lib/main.dart
+++ lib/main.dart
@@ -27,6 +27,49 @@ class Myprofile extends StatelessWidget {
           onPressed: () {},
           child: const Icon(Icons.add),
         ),
+
+        drawer: Drawer(
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: const <Widget>[
+              DrawerHeader(
+                decoration: BoxDecoration(
+                  color: Colors.blue,
+                ),
+                child: Text(
+                  'Drawer Header',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 24,
+                  ),
+                ),
+              ),
+              ListTile(leading: Icon(Icons.message),
+                title: Text('Messages'),),
+              ListTile(leading: Icon(Icons.account_circle),
+                title: Text('Profile'),
+              ),
+              ListTile(leading: Icon(Icons.settings),
+                title: Text('Settings'),
+              ),
+            ],
+          ),
+        ),
+
+        bottomNavigationBar: BottomNavigationBar(
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(icon: Icon(Icons.home),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(icon: Icon(Icons.business),
+              label: 'Business',
+            ),
+            BottomNavigationBarItem(icon: Icon(Icons.school),
+              label: 'School',
+            ),
+          ],
+        ),
+
       ),
       title: 'Flutter Demo',
       theme: ThemeData(
